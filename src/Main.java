5c4fe53c7026b397566f400275736d97dec059c9
HaarDD <maxgalitskiy@gmail.com>
2023-08-05 16:13:53 +0300

    new: add lection

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import base.PrintUtil;
 import base.TaskBase;
 import base.TasksSwitcher;
 import lecture1.*;
+import lecture2.L2Practice;
 
 
 public class Main {
@@ -12,7 +13,8 @@ public class Main {
         TasksSwitcher tasksSwitcher = new TasksSwitcher();
         tasksSwitcher.addLecture(new Lecture(1,"Введение в Java",
                 new TaskBase[]{new L1Task1(ptUl),new L1Task2and3(ptUl),new L1Task4(ptUl),new L1Task5(ptUl),new L1Task6(ptUl)}));
-
+        tasksSwitcher.addLecture(new Lecture(2,"Операторы управления",
+                new TaskBase[]{new L2Practice(ptUl)}));
         tasksSwitcher.runTasksSwitcher();
 
     }
