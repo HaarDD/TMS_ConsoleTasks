9c6122543ad5336c5107de5c386e4d794885a274
HaarDD <maxgalitskiy@gmail.com>
2023-08-06 04:46:48 +0300

    new: add tasks solutions to menu

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,16 @@ import base.Lecture;
 import base.TaskBase;
 import base.TasksSwitcher;
 import lecture1.*;
-import lecture2.L2Practice;
-
+import lecture2.*;
 
 public class Main {
     //Выполнил Максим Галицкий
     public static void main(String[] args) {
         TasksSwitcher tasksSwitcher = new TasksSwitcher();
         tasksSwitcher.addLecture(new Lecture(1,"Введение в Java",
-                new TaskBase[]{new L1Task1(),new L1Task2and3(),new L1Task4(),new L1Task5(),new L1Task6()}));
+                new TaskBase[]{new L1Practice(),new L1Task1(),new L1Task2and3(),new L1Task4(),new L1Task5(),new L1Task6star()}));
         tasksSwitcher.addLecture(new Lecture(2,"Операторы управления",
-                new TaskBase[]{new L2Practice()}));
+                new TaskBase[]{new L2Practice(), new L2Task1(), new L2Task2(), new L2Task3(), new L2Task4(), new L2Task5star()}));
         tasksSwitcher.runTasksSwitcher();
     }
 
