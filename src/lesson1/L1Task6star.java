10590955e4c913217770d9942d79d983d94fb9bc
HaarDD <maxgalitskiy@gmail.com>
2023-08-16 02:19:56 +0300

    fix: adapted to new names & remove number of task (it is done automatically now)

--- src/lesson1/L1Task6star.java
+++ src/lesson1/L1Task6star.java
@@ -1,7 +1,7 @@
 package lesson1;
 
-import base.PrintUtil;
-import base.TaskBase;
+import base.ConsoleUtil;
+import base.Runnable;
 
 /*Задача 6*. Написать программу которая будет менять местами значение целочисленных
         переменных. Пример:
@@ -9,20 +9,20 @@ import base.TaskBase;
 //код (ваше решение)
         sout(a); //выведет 2
         sout(b); //выведет 1*/
-public class L1Task6star extends TaskBase {
+public class L1Task6star extends Runnable {
 
     public L1Task6star() {
-        super(6, "Подмена содержимого переменных типа int");
+        super("Подмена содержимого переменных типа int");
     }
 
     @Override
-    public void runTask() {
-        int a = (int) PrintUtil.getNumber("Число a: "),
-                b = (int) PrintUtil.getNumber("Число b ");
-        PrintUtil.print("До подмены: a = " + a + " b = " + b);
+    public void run() {
+        int a = (int) ConsoleUtil.getNumber("Число a: "),
+                b = (int) ConsoleUtil.getNumber("Число b ");
+        ConsoleUtil.print("До подмены: a = " + a + " b = " + b);
         int temp = a;
         a = b;
         b = temp;
-        PrintUtil.print("После подмены: a = " + a + " b = " + b);
+        ConsoleUtil.print("После подмены: a = " + a + " b = " + b);
     }
 }
