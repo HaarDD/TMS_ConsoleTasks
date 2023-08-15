f265e066bcf5303f58870fc78d711f01ada38ac9
HaarDD <maxgalitskiy@gmail.com>
2023-08-16 02:24:47 +0300

    new: tree sort class for additional task (remove function will be soon)

---
+++ src/base/Tree.java
@@ -0,0 +1,53 @@
+package base;
+
+import java.util.List;
+
+public class Tree {
+    double value;
+    private Tree leftTree = null;
+    private Tree rightTree = null;
+
+    public Tree(double value) {
+        this.value = value;
+    }
+
+    public void insert(Tree tree) {
+        if (tree.value < value) {
+            if (leftTree != null) {
+                leftTree.insert(tree);
+            } else leftTree = tree;
+        } else {
+            if (rightTree != null) {
+                rightTree.insert(tree);
+            } else rightTree = tree;
+        }
+    }
+
+    public void traverseAscending(NodeAction action) {
+        if (leftTree != null) {
+            leftTree.traverseAscending(action);
+        }
+        action.act(this);
+        if (rightTree != null) {
+            rightTree.traverseAscending(action);
+        }
+    }
+
+    //TODO удаление числа
+
+    public interface NodeAction {
+        public void act(Tree tree);
+    }
+
+    public static class AddToSortedList implements NodeAction {
+        List<Double> list;
+
+        public AddToSortedList(List<Double> list) {
+            this.list = list;
+        }
+
+        public void act(Tree tree) {
+            list.add(tree.value);
+        }
+    }
+}
