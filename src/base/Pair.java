8c475ff2191d6ef8d46680afd7f4e74b567f110a
HaarDD <maxgalitskiy@gmail.com>
2023-08-16 02:27:31 +0300

    new: added pair class for easier work with ranges

---
+++ src/base/Pair.java
@@ -0,0 +1,22 @@
+package base;
+
+public class Pair {
+    private final Number first;
+    private final Number second;
+
+    Pair(Number from, Number second) {
+        this.first = from;
+        this.second = second;
+    }
+
+    public Number getFirst() {
+        return first;
+    }
+
+    public Number getSecond() {
+        return second;
+    }
+
+
+}
+
