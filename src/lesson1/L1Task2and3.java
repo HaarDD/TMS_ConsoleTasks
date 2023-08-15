652d044eda1f65b0708351d9e8b0de4b586d7347
HaarDD <maxgalitskiy@gmail.com>
2023-08-16 02:19:40 +0300

    fix: adapted to new names & remove number of task (it is done automatically now)

--- src/lesson1/L1Task2and3.java
+++ src/lesson1/L1Task2and3.java
@@ -1,24 +1,23 @@
 package lesson1;
 
-import base.PrintUtil;
-import base.TaskBase;
+import base.ConsoleUtil;
+import base.Runnable;
 
 /*Задача 2. В переменной n хранится двузначное число. Создайте программу, вычисляющую и
     выводящую на экран сумму цифр n. Например: n =26, в результате мы должны получить 8 (2+6).*/
 /*Задача 3. В переменной n хранится трёхзначное число. Создайте программу, вычисляющую и
         выводящую на экран сумму цифр n. Например: n =126, в результате мы должны получить
         9 (1+2+6).*/
-public class L1Task2and3 extends TaskBase {
+public class L1Task2and3 extends Runnable {
 
     public L1Task2and3() {
-        super(3, "(2-3) Сумма цифр числа");
+        super("(2-3) Сумма цифр числа");
     }
 
     @Override
-    public void runTask() {
-        PrintUtil.printOption(this);
-        int n = (int) PrintUtil.getNumber("Исходное число: ");
+    public void run() {
+        int n = (int) ConsoleUtil.getNumber("Исходное число: ");
         int sum = String.valueOf(n).chars().filter(Character::isDigit).map(Character::getNumericValue).sum();
-        PrintUtil.print("Сумма цифр числа \"" + n + "\": " + sum);
+        ConsoleUtil.print("Сумма цифр числа \"" + n + "\": " + sum);
     }
 }
