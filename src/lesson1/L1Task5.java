d88285a7bce2707810ed4573b35849e6e6cfcb91
HaarDD <maxgalitskiy@gmail.com>
2023-08-16 02:19:52 +0300

    fix: adapted to new names & remove number of task (it is done automatically now)

--- src/lesson1/L1Task5.java
+++ src/lesson1/L1Task5.java
@@ -1,21 +1,21 @@
 package lesson1;
 
-import base.PrintUtil;
-import base.TaskBase;
+import base.ConsoleUtil;
+import base.Runnable;
 
 /*Задача 5. В переменных q и w хранятся два натуральных числа. Создайте программу, выводящую
         на экран результат деления q на w с остатком. Пример вывода программы (для случая,
         когда в q хранится 21, а в w хранится 8): 21 / 8 = 2 и 5 в остатке.*/
-public class L1Task5 extends TaskBase {
+public class L1Task5 extends Runnable {
 
     public L1Task5() {
-        super(5, "Результат деления с остатком");
+        super("Результат деления с остатком");
     }
 
     @Override
-    public void runTask() {
-        int q = (int) PrintUtil.getNaturalNumber("Введите натуральное число q: "),
-                w = (int) PrintUtil.getNaturalNumber("Введите натуральное число w: ");
-        PrintUtil.print("Деление с остатком: " + q + " / " + w + " = " + (q / w) + " и " + (q % w) + " в остатке");
+    public void run() {
+        int q = (int) ConsoleUtil.getNaturalNumber("Введите натуральное число q: "),
+                w = (int) ConsoleUtil.getNaturalNumber("Введите натуральное число w: ");
+        ConsoleUtil.print("Деление с остатком: " + q + " / " + w + " = " + (q / w) + " и " + (q % w) + " в остатке");
     }
 }
