71146c77eec8ef543a17fbe571003edf9d431960
HaarDD <maxgalitskiy@gmail.com>
2023-08-16 02:19:27 +0300

    fix: adapted to new names & remove number of task (it is done automatically now)

--- src/lesson1/L1Task1.java
+++ src/lesson1/L1Task1.java
@@ -1,20 +1,19 @@
 package lesson1;
 
-import base.PrintUtil;
-import base.TaskBase;
+import base.ConsoleUtil;
+import base.Runnable;
 
 /* Задача 2. Написать приложение, которое будет вычислять и выводить значение по формуле:
 a=4*(b+c-1)/2; b и c задаем в коде самостоятельно.*/
-public class L1Task1 extends TaskBase {
+public class L1Task1 extends Runnable {
     public L1Task1() {
-        super(2, "Вычисление по формуле a=4*(b+c-1)/2");
+        super("Вычисление по формуле a=4*(b+c-1)/2");
     }
 
     @Override
-    public void runTask() {
-        PrintUtil.printOption(this);
-        Double b = PrintUtil.getNumber("Число b: "), c = PrintUtil.getNumber("Число c: ");
+    public void run() {
+        Double b = ConsoleUtil.getNumber("Число b: "), c = ConsoleUtil.getNumber("Число c: ");
         double result = (4 * (b + c - 1) / 2);
-        PrintUtil.print("Результат: 4 * (" + b + " + " + c + " - 1) / 2 = " + result);
+        ConsoleUtil.print("Результат: 4 * (" + b + " + " + c + " - 1) / 2 = " + result);
     }
 }
