3b50b91990d84ea0ab49181a8f0e56b8fd5cf770
HaarDD <maxgalitskiy@gmail.com>
2023-09-25 04:33:55 +0300

    add: additional task (braces nesting) solution with deque

---
+++ src/lesson14_extended/BracesTaskMethodDeque.java
@@ -0,0 +1,45 @@
+package lesson14_extended;
+
+import base.ConsoleUtil;
+import base.Runnable;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BracesTaskMethodDeque extends Runnable {
+
+    public BracesTaskMethodDeque() {
+        super("Правильность вложенности скобок разных видов в строку с Deque");
+    }
+
+    @Override
+    public void run() {
+        Map<Character, Character> braces = new HashMap<>();
+        braces.put('(', ')');
+        braces.put('<', '>');
+        braces.put('{', '}');
+
+        String inputString = "(sd(fdsf(sdfdsfsdf)sdfsdf(<dsfdsf>cz12412fs)sdfsdf)";
+        ConsoleUtil.println("Введенная строка: " + inputString);
+        ConsoleUtil.println(checkBraces(inputString, braces) ? "Порядок скобок верный" : "Порядок скобок НЕверный");
+    }
+
+    public boolean checkBraces(String inputString, Map<Character, Character> bracesMap) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : inputString.toCharArray()) {
+            if (bracesMap.containsKey(c)) {
+                stack.push(c);
+            } else if (bracesMap.containsValue(c)) {
+                if (stack.isEmpty() || bracesMap.get(stack.pop()) != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+
+}
