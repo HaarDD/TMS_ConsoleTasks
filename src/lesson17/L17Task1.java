3735678ec3f87146d113d3bc8cdefa9dca12cea1
HaarDD <maxgalitskiy@gmail.com>
2023-10-02 23:10:32 +0300

    add: lesson 17 task 1 solution (abbreviation search)

---
+++ src/lesson17/L17Task1.java
@@ -0,0 +1,38 @@
+package lesson17;
+
+import base.ConsoleUtil;
+import base.Runnable;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/*Задача *:
+Задача 1:
+Вывести в консоль из строки которую пользователь вводит с клавиатуры все
+аббревиатуры. Аббревиатурой будем считать слово от 2 до 6 символов, состоящее
+только из прописных букв, без чисел*/
+public class L17Task1 extends Runnable {
+
+    public static final Pattern regexAllNumbers = Pattern.compile("\\p{Lu}{2,6}");
+
+    public L17Task1() {
+        super("Поиск аббревиатур");
+    }
+
+    @Override
+    public void run() {
+        String regexTestCase = "NASA (National Aeronautics and Space Administration) – Американская организация, занимающаяся исследованием космоса и астрономией." +
+                " UNESCO (United Nations Educational, Scientific and Cultural Organization) – Организация Объединенных Наций, посвященная образованию, науке" +
+                " и культуре, с целью содействия миру и сотрудничеству между нациями. FIFA (Fédération Internationale de Football Association) – " +
+                "Международная футбольная ассоциация, управляющая мировым футболом и организующая Чемпионаты мира по футболу.";
+
+        Matcher matcher = regexAllNumbers.matcher(regexTestCase);
+        StringBuilder resultStringBuilder = new StringBuilder();
+        while (matcher.find()) {
+            resultStringBuilder.append(matcher.group()).append('\n');
+        }
+        ConsoleUtil.println("Исходный текст: " + regexTestCase + "\nНайденные аббревиатуры: \n" + resultStringBuilder.substring(0, resultStringBuilder.length() - 1));
+    }
+
+}
